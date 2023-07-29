7fe9076df39c4cb584790a95abdd3ca420ea6604
Hussein Najah <husseinnajah123@gmail.com>
2023-07-29 22:04:29 +0300

    herp

--- components/hero.tsx
+++ components/hero.tsx
@@ -5,30 +5,32 @@ import Image from "next/image";
 import Button from "@/components/button";
 
 const Hero = () => {
-  const handelScroll = () => {};
+  const handleScroll = () => {};
 
   return (
     <div className="hero">
       <div className="flex-1 pt-36 padding-x">
         <h1 className="hero__title">
-          Find, Book, or rent a car — quickly and easily!
+          Find, book, rent a car—quick and super easy!
         </h1>
+
         <p className="hero__subtitle">
           Streamline your car rental experience with our effortless booking
-          proses
+          process.
         </p>
+
         <Button
           title="Explore Cars"
-          containerStyles="bg-primary-blue text-white rounded-full mt-10"
-          handleCLick={handelScroll}
-          disabled={false}
+          className="bg-primary-blue text-white rounded-full mt-10"
+          handleClick={handleScroll}
         />
       </div>
       <div className="hero__image-container">
         <div className="hero__image">
-          <Image fill alt="Car Image" src="/hero.png" />
-          <div className="hero__image-overlay"></div>
+          <Image src="/hero.png" alt="hero" fill className="object-contain" />
         </div>
+
+        <div className="hero__image-overlay" />
       </div>
     </div>
   );
