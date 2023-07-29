3a2dd60dba567c92854c3ff9d3c5e4642cb2d91f
Hussein Najah <husseinnajah123@gmail.com>
2023-07-29 20:02:05 +0300

    reat init

--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
             href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
             target="_blank"
             rel="noopener noreferrer"
-          >
+          >z
             By{' '}
             <Image
               src="/vercel.svg"
