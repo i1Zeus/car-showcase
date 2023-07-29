0d652f487d693674ba8e6f749403c9120c60ecdb
Hussein Najah <husseinnajah123@gmail.com>
2023-07-29 21:43:50 +0300

    hero ;

--- components/hero.tsx
+++ components/hero.tsx
@@ -26,7 +26,8 @@ const Hero = () => {
       </div>
       <div className="hero__image-container">
         <div className="hero__image">
-          <Image />
+          <Image fill alt="Car Image" src="/hero.png" />
+          <div className="hero__image-overlay"></div>
         </div>
       </div>
     </div>
