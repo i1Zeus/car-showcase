772433efaf90831df20d15ac2eaa667eb4a35a56
Hussein Najah <husseinnajah123@gmail.com>
2023-07-30 16:47:00 +0300

    api fetch cars

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,21 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export async function fetchCars() {
+  const headers = {
+    "X-RapidAPI-Key": "93903b0e19mshaacbd86b6cbf924p1d9223jsnc163fb8631c2",
+    "X-RapidAPI-Host": "cars-by-api-ninjas.p.rapidapi.com",
+  };
+
+  const response = await fetch(
+    "https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla",
+    {
+      headers: headers,
+    }
+  );
+
+  const results = await response.json();
+
+  return results;
+}
