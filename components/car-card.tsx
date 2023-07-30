aac5d5b3d153c3b416567b43a68bf054d7a449e4
Hussein Najah <husseinnajah123@gmail.com>
2023-07-30 17:46:14 +0300

    rename

---
+++ components/car-card.tsx
@@ -0,0 +1,96 @@
+"use client";
+
+import { calculateCarRent } from "@/lib/utils";
+import Image from "next/image";
+import { useState } from "react";
+import Button from "./button";
+import CarDetails from "./car-details";
+
+export interface CarProps {
+  city_mpg: number;
+  class: string;
+  combination_mpg: number;
+  cylinders: number;
+  displacement: number;
+  drive: string;
+  fuel_type: string;
+  highway_mpg: number;
+  make: string;
+  model: string;
+  transmission: string;
+  year: number;
+}
+
+interface CarCardProps {
+  car: CarProps;
+}
+
+const CarCard = ({ car }: CarCardProps) => {
+  const { city_mpg, year, make, model, transmission, drive } = car;
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  const carRent = calculateCarRent(city_mpg, year);
+
+  return (
+    <div className="car-card group">
+      <div className="car-card__content">
+        <h2 className="car-card__content-title">
+          {make} {model}
+        </h2>
+      </div>
+      <p className="flex mt-6 text-3xl font-extrabold">
+        <span className="self-start text-[14px] font-semibold">$</span>
+        {carRent}
+        <span className="self-end text-[14px] font-medium">/day</span>
+      </p>
+      <div className="relative w-full h-40 my-3 object-contain">
+        <Image
+          src="/hero.png"
+          fill
+          priority
+          alt="car image"
+          className="object-contain"
+        />
+      </div>
+      <div className="relative flex w-full mt-2">
+        <div className="flex group-hover:invisible w-full justify-between text-gray">
+          <div className="flex flex-col justify-center items-center gap-2">
+            <Image
+              src="/steering-wheel.svg"
+              width={20}
+              height={20}
+              alt="Steering Wheel"
+            />
+            <p className="text-[14px]">
+              {transmission === "a" ? "Automatic" : "Manual"}
+            </p>
+          </div>
+          <div className="flex flex-col justify-center items-center gap-2">
+            <Image src="/tire.svg" width={20} height={20} alt="Tire" />
+            <p className="text-[14px]">{drive.toUpperCase()}</p>
+          </div>
+          <div className="flex flex-col justify-center items-center gap-2">
+            <Image src="/gas.svg" width={20} height={20} alt="Gas" />
+            <p className="text-[14px]">{city_mpg} MPG</p>
+          </div>
+        </div>
+        <div className="car-card__btn-container">
+          <Button
+            title="View More"
+            className="w-full py-[16px] rounded-full bg-primary-blue text-white leading-[14px] font-bold"
+            rightIcon="/right-arrow.svg"
+            handleClick={() => setIsOpen(!isOpen)}
+          />
+        </div>
+      </div>
+      <CarDetails
+        isOpen={isOpen}
+        closeModal={() => setIsOpen(false)}
+        car={car}
+      />
+    </div>
+  );
+};
+
+export default CarCard;
