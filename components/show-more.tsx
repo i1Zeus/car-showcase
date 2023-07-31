a842605718fccc2e8cf3c9349f4b27b07f4d10be
Hussein Najah <husseinnajah123@gmail.com>
2023-07-31 15:30:33 +0300

    show more

--- components/show-more.tsx
+++ components/show-more.tsx
@@ -1,7 +1,34 @@
-const ShowMore = () => {
-  return (
-    <div>ShowMore</div>
-  )
+"use client";
+
+import { updateSearchParams } from "@/lib/utils";
+import Button from "./button";
+import { useRouter } from "next/navigation";
+
+interface ShowMoreProps {
+  pageNumber: number;
+  isNext: boolean;
 }
 
-export default ShowMore
+const ShowMore = ({ pageNumber, isNext }: ShowMoreProps) => {
+  const router = useRouter();
+  const handleNavigation = () => {
+    const newLimit = (pageNumber + 1) * 10;
+    const newPathName = updateSearchParams("limit", `${newLimit}`);
+    router.push(newPathName);
+  };
+
+  return (
+    <div className="w-full flex-center gap-5 mt-10">
+      {!isNext && (
+        <Button
+          title="Show More"
+          type="button"
+          className="bg-primary-blue rounded-full text-white"
+          handleClick={handleNavigation}
+        />
+      )}
+    </div>
+  );
+};
+
+export default ShowMore;
